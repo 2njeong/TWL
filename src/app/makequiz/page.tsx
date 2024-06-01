4962b1316ecbb206ace09f5cd6d9210ba1875c27
injeong Hwang <twilight9758@gmail.com>
2024-06-01 16:55:44 +0900

    [feat] - quiz, makequiz 폴더 생성

---
+++ src/app/makequiz/page.tsx
@@ -0,0 +1,5 @@
+const MakeQuizPage = () => {
+  return <div>page</div>;
+};
+
+export default MakeQuizPage;
---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,5 @@
+const QuizPage = () => {
+  return <div>page</div>;
+};
+
+export default QuizPage;
