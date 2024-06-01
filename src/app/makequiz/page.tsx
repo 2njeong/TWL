15436d643c52227abbc4c3b138b210daadae918f
injeong Hwang <twilight9758@gmail.com>
2024-06-01 19:09:43 +0900

    [feat] - react.icon 설치 + makeQuizPage 틀 생성

--- src/app/makequiz/page.tsx
+++ src/app/makequiz/page.tsx
@@ -1,5 +1,26 @@
+import SelectInput from ' @/components/SelectInput';
+
 const MakeQuizPage = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center w-full h-screen bg-gray-100 p-8">
+        <div className="w-full max-w-[1000px] relative">
+          <section className="w-full max-w-[1000px] flex justify-end absolute -top-5">
+            <button className="bg-pinkOne rounded">객관식</button>
+            <button className="bg-yelThree rounded">주관식</button>
+          </section>
+          <div className="bg-beiOne w-full flex flex-col items-center p-2 absolute gap-2">
+            <section>
+              <input placeholder="질문"></input>
+            </section>
+            <section>
+              <SelectInput />
+            </section>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MakeQuizPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        yelOne: '#FFF6BD',
+        yelTwo: '#FCED88',
+        yelThree: '#FDFCB8',
+        yelFour: '#F9F6AA',
+        pinkOne: '#F2E9E9',
+        beiOne: '#E1DAB9'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
