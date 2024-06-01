85cd5ac8f9ab0adb782434629fc46569ae635d4c
injeong Hwang <twilight9758@gmail.com>
2024-06-02 02:03:25 +0900

    [refactor] - SelectKindOfQuiz.tsx로 리팩토링

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import QueryProvider from './provider/QueryProvider';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <QueryProvider>
-          {children}
+          <RecoilRoot> {children}</RecoilRoot>
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryProvider>
       </body>
