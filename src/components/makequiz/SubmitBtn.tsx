0dc634983c7facd810a4ba566b546f4f8a3f94ec
injeong Hwang <twilight9758@gmail.com>
2024-06-04 15:21:07 +0900

    [style] - 문제 만들기 button 스타일 수정

--- src/components/makequiz/SubmitBtn.tsx
+++ src/components/makequiz/SubmitBtn.tsx
@@ -6,9 +6,11 @@ const SubmitBtn = () => {
   const { pending } = useFormStatus();
 
   return (
-    <button disabled={pending} className={`opacity-${pending ? '50' : '1'}`}>
-      {pending ? '문제를 만드는 중...' : '문제 만들기'}
-    </button>
+    <section className="w-full flex justify-center">
+      <button disabled={pending} className={`opacity-${pending ? '50' : '1'} w-3/6 border rounded`}>
+        {pending ? '문제를 만드는 중...' : '문제 만들기'}
+      </button>
+    </section>
   );
 };
 
