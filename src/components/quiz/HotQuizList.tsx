d5175825906f5ea67fb489537831c2b846a692c9
injeong Hwang <twilight9758@gmail.com>
2024-06-12 21:08:42 +0900

    [fix] - 드래그 구현 보완 및 리팩토링

--- src/components/quiz/HotQuizList.tsx
+++ src/components/quiz/HotQuizList.tsx
@@ -20,49 +20,30 @@ const HotQuizList = () => {
   const [position, setPosition] = useState(0);
   const [startMoveX, setStartMoveX] = useState(0);
   const [mouseDown, setMouseDown] = useState(false);
+  const [movingX, setMovingX] = useState(0);
+  const containerWidth = containerRef?.current?.getBoundingClientRect().width;
 
-  console.log('start--------------------------');
-  console.log('mouseDown =>', mouseDown);
-  console.log('startMoveX =>', startMoveX);
-  console.log('position =>', position);
-  console.log('마지막 =>', testRef.current[19]?.getClientRects()[0].right);
-  console.log('이거봐바', containerRef.current?.getBoundingClientRect());
-  console.log('저거봐바', containerRef.current?.scrollWidth);
-
-  // useEffect(() => {
-  //   if (testRef.current[19] && testRef.current[19].getClientRects()[0].right < 1000) {
-  //     testRef.current[19]?.getClientRects()[0].right === 1000;
-  //   }
-  // }, [position, mouseDown]);
-
-  const handleMouseDown = (clickEvent: React.MouseEvent<Element, MouseEvent>, idx: number) => {
-    console.log('idx =>', idx);
-    setStartMoveX(clickEvent.screenX);
+  const handleMouseDown = (clickEvent: React.MouseEvent<Element, MouseEvent>) => {
     setMouseDown(true);
+    setStartMoveX(clickEvent.screenX);
+  };
+
+  const requestAnimation = () => {
+    requestAnimationFrame(() => {
+      if (!containerRef.current) return;
+      containerRef.current.style.transform = `translateX(${position}px)`;
+      containerRef.current.style.transition = 'transform 0.2s ease-in-out';
+    });
   };
 
   const handleMouseMove = (moveEvent: React.MouseEvent<Element, MouseEvent>) => {
     if (mouseDown) {
       const deltaX = moveEvent.screenX - startMoveX;
-      setPosition((prev) => {
-        console.log('prev =>', prev);
-        return prev + deltaX;
-      }); // 이전 위치에 이동 거리를 더함
-      setStartMoveX(moveEvent.screenX); //
-      requestAnimationFrame(() => {
-        if (!containerRef.current) return;
-        containerRef.current.style.transform = `translateX(${position}px)`;
-        containerRef.current.style.transition = 'transform 0.2s ease-in-out';
-      });
+      setPosition((prev) => prev + deltaX); // 이전 위치에 이동 거리를 더함
+      setStartMoveX(moveEvent.screenX);
+      requestAnimation();
     } else {
-      // position ?? setPosition(-3200);
-      requestAnimationFrame(() => {
-        if (!containerRef.current) return;
-        containerRef.current.style.transform = `translateX(${
-          position < -3200 ? -3200 : position > 40 ? 35 : position
-        }px)`;
-        containerRef.current.style.transition = 'transform 0.2s ease-in-out';
-      });
+      requestAnimation();
       position < -3200 ? setPosition(-3200) : position > 40 ? setPosition(40) : null;
     }
   };
@@ -76,11 +57,6 @@ const HotQuizList = () => {
       <div
         ref={containerRef}
         className="flex flex-nowrap gap-4 h-[300px] w-max overflow-x-visible"
-        // style={{
-        //   transform: `translateX(${position > 40 ? 35 : position}px)`,
-        //   transition: 'transform 0.2s ease-in-out'
-        // }}
-
         onMouseUp={handleMouseUp}
       >
         {quizList?.map((quiz, idx) => (
@@ -88,7 +64,7 @@ const HotQuizList = () => {
             ref={(el: any) => (testRef.current[idx] = el)}
             key={quiz.quiz_id}
             className="h-48 w-48 bg-yelTwo select-none"
-            onMouseDown={(e) => handleMouseDown(e, idx)}
+            onMouseDown={handleMouseDown}
             onMouseMove={handleMouseMove}
           >
             {quiz.question}
