29407da73f45bf6bdf8add69d755e5911c907239
injeong Hwang <twilight9758@gmail.com>
2024-07-06 02:57:01 +0900

    [feat] - 랜덤으로 사과 위치 잡기 문서 기준 절대 위치

--- src/app/algorithm/page.tsx
+++ src/app/algorithm/page.tsx
@@ -1,37 +1,67 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const AlgorithmPage = () => {
+  const preApples = ['a', 'b', 'c', 'd', 'e'];
+  const [apples, setApples] = useState<{ appleX: number; appleY: number }[]>([]);
   const treeRef = useRef<HTMLDivElement | null>(null);
+  const applesRef = useRef<(HTMLDivElement | null)[]>([]);
+
+  function getRandomInt(min: number, max: number) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  }
+
   useEffect(() => {
     if (treeRef.current) {
-      console.log('left =>', treeRef.current?.getBoundingClientRect().left);
-      console.log('좌측 가장자리 X좌표 =>', treeRef.current?.getBoundingClientRect().left + window.scrollX);
-      console.log('right =>', treeRef.current?.getBoundingClientRect().right);
-      console.log('우측 가장자리 X좌표 =>', treeRef.current?.getBoundingClientRect().right + window.scrollX);
-      console.log('bottom  =>', treeRef.current?.getBoundingClientRect().bottom);
-      console.log('하단 가장자리 Y좌표  =>', treeRef.current?.getBoundingClientRect().bottom + window.scrollY);
-      console.log('top =>', treeRef.current?.getBoundingClientRect().top);
-      console.log('상단 가장자리 Y좌표 =>', treeRef.current?.getBoundingClientRect().top + window.scrollY);
+      const leftEndX = treeRef.current?.getBoundingClientRect().left + window.scrollX;
+      const rightEndX = treeRef.current?.getBoundingClientRect().right + window.scrollX;
+      const topEndY = treeRef.current?.getBoundingClientRect().top + window.scrollY;
+      const bottomEndY = treeRef.current?.getBoundingClientRect().bottom + window.scrollY;
+
+      const appleXYArr = preApples.reduce((acc, _) => {
+        let appleX: number, appleY: number, appleXY;
+
+        do {
+          appleX = getRandomInt(leftEndX, rightEndX);
+          appleY = getRandomInt(topEndY, bottomEndY);
+          appleXY = { appleX, appleY };
+        } while (acc.some((item) => item.appleX === appleX && item.appleY === appleY));
+        acc.push(appleXY);
+
+        return acc;
+      }, [] as { appleX: number; appleY: number }[]);
+
+      console.log('appleXYArr =>', appleXYArr);
+      setApples(appleXYArr);
     }
-
-    // console.log('height  =>', treeRef.current?.getBoundingClientRect().height);
-    // console.log('width  =>', treeRef.current?.getBoundingClientRect().width);
-    // console.log('x  =>', treeRef.current?.getBoundingClientRect().x);
-    // console.log('y  =>', treeRef.current?.getBoundingClientRect().y);
     console.log('----------------------------------------------');
-  }, []);
+  }, [treeRef.current]);
 
-  const getRandomPosition = (min: number, max: number) => {
-    return Math.random() * (max - min) + min;
-  };
+  useEffect(() => {
+    console.log('appleXYArr =>', apples);
+  }, [apples]);
 
   return (
     <div className="w-full flex justify-center">
-      <div ref={treeRef} className="w-[500px] h-[450px] bg-gray-200">
+      <div ref={treeRef} className="w-[500px] h-[450px] bg-gray-200 relative">
         트리영역
       </div>
+      {apples.map((apple, index) => (
+        <div
+          key={index}
+          className={`absolute w-10 h-10 left-${apple.appleX} top-${apple.appleY}`}
+          style={{
+            position: 'absolute',
+            left: `${apple.appleX}px`,
+            top: `${apple.appleY}px`,
+            backgroundColor: 'red',
+            borderRadius: '50%'
+          }}
+        ></div>
+      ))}
     </div>
   );
 };
